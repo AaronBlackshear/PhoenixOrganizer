88c85547355b54805bf01d702ba5b4ad2370b5ef
AaronBlackshear <ablackshear7820@gmail.com>
2018-11-28 10:29:20 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Calendar from './components/Calendar';
 import Login from './components/LoginForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Calendar /> */}
         <Login />
       </div>
     );
