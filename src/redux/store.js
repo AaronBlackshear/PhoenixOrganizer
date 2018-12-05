684f449dc038c8cb8df232f934aac1efaf2efd56
AaronBlackshear <ablackshear7820@gmail.com>
2018-12-04 20:33:50 -0600

    configured redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,22 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
-
 import promiseMiddleware from 'redux-promise-middleware'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './reducers/userReducer'
 import calendarReducer from './reducers/calendarReducer'
 
-// export default store;
-export default
-  createStore(
-    combineReducers({ userReducer, calendarReducer }), applyMiddleware(promiseMiddleware())
-  );
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+  userReducer,
+  calendarReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(promiseMiddleware()));
+export const persistor = persistStore(store);
